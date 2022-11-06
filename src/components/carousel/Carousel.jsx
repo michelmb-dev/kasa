785ab03c21acdd0f455d065f224fa150d54d0dbb
Michel B <michel.mb.dev@gmail.com>
2022-11-06 20:59:13 +0100

    feat: create Carousel component.

---
+++ src/components/carousel/Carousel.jsx
@@ -0,0 +1,44 @@
+import styles from "./Carousel.module.css";
+import {useState} from "react";
+
+
+function Carousel({arrayImages}) {
+  const [currentSlide, setCurrentSlide] = useState(0);
+
+   const prevSlide = () => {
+    setCurrentSlide(
+      currentSlide === 0 ? arrayImages.length -1 : currentSlide -1
+    )
+  }
+
+  const nextSlide = () => {
+    setCurrentSlide(
+      currentSlide === arrayImages.length -1 ? 0 : currentSlide + 1
+    )
+  }
+
+  return (
+		<div className={styles.container}>
+        <div className={styles.slide}>
+          <img className={styles.image} src={arrayImages[currentSlide]} alt="aperçu du logement"/>
+          <p className={styles.info}>{currentSlide + 1} / {arrayImages.length}</p>
+        </div>
+      {arrayImages.length > 1 && (
+        <>
+          <span className={styles.btn + " " + styles.prevSlide} onClick={prevSlide}>
+            <svg fill="#FFFFFF" width="48" height="80" viewBox="0 0 48 80" xmlns="http://www.w3.org/2000/svg">
+              <path d="M47.04 7.78312L39.92 0.703125L0.359985 40.3031L39.96 79.9031L47.04 72.8231L14.52 40.3031L47.04 7.78312Z" />
+            </svg>
+          </span>
+          <span className={styles.btn + " " + styles.nextSlide} onClick={nextSlide}>
+            <svg fill="#FFFFFF" width="48" height="80" viewBox="0 0 48 80"  xmlns="http://www.w3.org/2000/svg">
+              <path d="M0.960022 72.3458L8.04002 79.4258L47.64 39.8258L8.04002 0.22583L0.960022 7.30583L33.48 39.8258L0.960022 72.3458V72.3458Z"/>
+            </svg>
+          </span>
+        </>
+      )}
+		</div>
+	);
+}
+
+export default Carousel;
