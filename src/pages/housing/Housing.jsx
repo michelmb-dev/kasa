516f7f52fbea41e0f1726bad0de7709b0425e2c9
Michel B <michel.mb.dev@gmail.com>
2022-11-10 14:20:15 +0100

    feat:create housing page.

--- src/pages/housing/Housing.jsx
+++ src/pages/housing/Housing.jsx
@@ -1,5 +1,11 @@
 import {useNavigate, useParams} from "react-router-dom";
 import {useEffect, useState} from "react";
+import Profil from "../../components/profil/Profil";
+import Stars from "../../components/stars/Stars";
+import styles from "./Housing.module.css";
+import Carousel from "../../components/carousel/Carousel";
+import Tags from "../../components/tags/Tags";
+import Dropdown from "../../components/dropdown/Dropdown";
 
 function Housing() {
 	const {id} = useParams();
@@ -17,12 +23,38 @@ function Housing() {
 				}).catch(() => navigate("/error"));
 		}
 		getHousing();
-	},[id, navigate])
+	},[id, navigate]);
 
+
+	const {pictures, rating, host, tags, description, equipments} = housing;
 	return (
-		<>
-			<div>{housing.title}</div>
-		</>
+			<>
+				{pictures &&
+				<Carousel arrayImages={pictures}/>
+				}
+				<div className={styles.headerWrapper}>
+					<div className={styles.title}>
+						<h2>{housing.title}</h2>
+						<p>{housing.location}</p>
+						{tags && <Tags tags={tags}/>}
+					</div>
+					<div className={styles.author}>
+						<Profil host={host}/>
+						<Stars rating={rating}/>
+					</div>
+				</div>
+				<div className={styles.bodyWrapper}>
+					<Dropdown title="Description">
+						<p className={styles.text}>{description}</p>
+					</Dropdown>
+					<Dropdown title="Équipements">
+						{equipments && equipments.map((text, index) => (
+							<p className={styles.text} key={index}>{text}</p>
+						))}
+					</Dropdown>
+				</div>
+			</>
+
 	);
 }
 
