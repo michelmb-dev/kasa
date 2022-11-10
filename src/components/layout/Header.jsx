ff43d4581813c5af0591012d2c4712937c2a2091
Michel B <michel.mb.dev@gmail.com>
2022-11-10 15:01:14 +0100

    fix: classname with template literal.

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -7,10 +7,10 @@ function Header() {
 		<header className={styles.header}>
 			<Link to="/" aria-label="Accueil"><img src={logo} alt="logo du site Kasa"/></Link>
 			<nav className={styles.nav}>
-				<NavLink className={({isActive}) => isActive ? styles.link + " " + styles.linkActive : styles.link } end to="/">
+				<NavLink className={({isActive}) => isActive ? `${styles.link} ${styles.linkActive}` : styles.link } end to="/">
 					accueil
 				</NavLink>
-				<NavLink className={({isActive}) => isActive ? styles.link + " " + styles.linkActive : styles.link }
+				<NavLink className={({isActive}) => isActive ? `${styles.link} ${styles.linkActive}` : styles.link }
 					end to="/about">
 					à propos
 				</NavLink>
