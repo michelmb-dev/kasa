f60fecec0c466c31a2f1dfbd08b94d38a1e26b39
Michel B <michel.mb.dev@gmail.com>
2022-11-21 11:35:59 +0100

    refactor: rename component

---
+++ src/components/banner/Banner.jsx
@@ -0,0 +1,12 @@
+import styles from "./Banner.module.css";
+
+function Banner({ background, styleHeight, children }) {
+	return (
+    <section className={`${styles.wrapper} ${styleHeight}`}>
+      <img src={background} alt="Vue sur les montagnes"/>
+      {children}
+    </section>
+	);
+}
+
+export default Banner;
