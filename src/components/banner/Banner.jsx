efa384d23f79408dd168f43ca728495a7cccfc48
Michel B <michel.mb.dev@gmail.com>
2022-11-21 12:17:59 +0100

    fix: undefined style on component

--- src/components/banner/Banner.jsx
+++ src/components/banner/Banner.jsx
@@ -2,7 +2,7 @@ import styles from "./Banner.module.css";
 
 function Banner({ background, styleHeight, children }) {
 	return (
-    <section className={`${styles.wrapper} ${styleHeight}`}>
+    <section className={`${styles.wrapper} ${styleHeight !== undefined ? styleHeight : ""}`}>
       <img src={background} alt="Vue sur les montagnes"/>
       {children}
     </section>
