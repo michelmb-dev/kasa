42225ecc9c463cc95915345820637049e2cf71b6
michelmb-dev <michel.mb.dev@gmail.com>
2023-10-20 14:29:52 +0200

    update: change react-scripts to vitejs

--- src/components/banner/Banner.jsx
+++ src/components/banner/Banner.jsx
@@ -1,12 +1,16 @@
-import styles from "./Banner.module.css";
+import styles from './Banner.module.css'
 
 function Banner({ background, styleHeight, children }) {
-	return (
-    <section className={`${styles.wrapper} ${styleHeight !== undefined ? styleHeight : ""}`}>
-      <img src={background} alt="Vue sur les montagnes"/>
+  return (
+    <section
+      className={`${styles.wrapper} ${
+        styleHeight !== undefined ? styleHeight : ''
+      }`}
+    >
+      <img src={background} alt='Vue sur les montagnes' />
       {children}
     </section>
-	);
+  )
 }
 
-export default Banner;
+export default Banner
